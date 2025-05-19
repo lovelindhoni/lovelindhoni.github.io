928241432b6e5ca72d99d1015fd2180c8c52f640
Lovelin <100030865+lovelindhoni@users.noreply.github.com>
2025-05-19 18:48:51 +0530

    feat: nix flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "lovelindhoni's site development environment flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.zola pkgs.minify pkgs.imagemagick ];
+        };
+      });
+}
